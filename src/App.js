586a0896164008bddc36274de3c7ddb645025c89
IvettaDashkova <ivettazakonno@gmail.com>
2021-03-19 01:21:36 +0200

    test linting

---
+++ src/App.js
@@ -0,0 +1,17 @@
+//import React from 'react';
+import Profile from './components/Profile';
+import user from './user.json';
+
+function App() {
+  return (
+    <Profile
+      avatar={user.avatar}
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      stats={user.stats}
+    />
+  );
+}
+
+export default App;
---
+++ src/components/Profile.js
@@ -0,0 +1,46 @@
+//import React from 'react';
+import PropTypes from 'prop-types';
+import defaultImg from '../components/default.svg';
+
+const Profile = ({ avatar, name, tag, location, stats }) => (
+  <div>
+    <div>
+      <img src={avatar} alt="Аватар пользователя" />
+      <p>{name}</p>
+      <p>{tag}</p>
+      <p>{location}</p>
+    </div>
+
+    <ul>
+      <li>
+        <span>Followers</span>
+        <span>{stats.followers}</span>
+      </li>
+      <li>
+        <span>Views</span>
+        <span>{stats.views}</span>
+      </li>
+      <li>
+        <span>Likes</span>
+        <span>{stats.likes}</span>
+      </li>
+    </ul>
+  </div>
+);
+Profile.defaultProps = {
+  avatar: defaultImg,
+  location: 'Citizen of the world',
+};
+
+Profile.propTypes = {
+  avatar: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string,
+  stats: PropTypes.shape({
+    followers: PropTypes.number,
+    views: PropTypes.number,
+    likes: PropTypes.number,
+  }).isRequired,
+};
+export default Profile;
