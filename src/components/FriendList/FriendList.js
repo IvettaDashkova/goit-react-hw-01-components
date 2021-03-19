75aeee79d58726aa9099532cc9832e92ed8c4c8d
IvettaDashkova <ivettazakonno@gmail.com>
2021-03-20 00:24:50 +0200

    add task 3

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,10 +1,11 @@
+import PropTypes from 'prop-types';
 import FriendListItem from '../FriendListItem/FriendListItem'
 
 const FriendList = ({ friends }) => (
-  <ul class="friend-list">
+  <ul>
     {friends.map(friend => (
-        <FriendListItem
-            id={friend.id}
+      <FriendListItem
+            key={friend.id.toString()}
             avatar={friend.avatar}
             name={friend.name}
             isOnline={friend.isOnline}
@@ -12,4 +13,9 @@ const FriendList = ({ friends }) => (
     ))}
   </ul>
 );
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
 export default FriendList;
--- src/components/FriendListItem/FriendListItem.js
+++ src/components/FriendListItem/FriendListItem.js
@@ -1,8 +1,28 @@
-const FriendListItem = ({ id, avatar, name, isOnline }) => (
-  <li key={id}>
-    <span>{isOnline}</span>
-    <img src={avatar} alt="" width="48" />
-    <p>{name}</p>
-  </li>
-);
+//import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './FriendListItem.module.css';
+import defaultImg from './default.svg';
+
+const FriendListItem = ({ id, avatar, name, isOnline }) => {
+  const spanClass = isOnline ? styles.active : styles.disabled;
+  return (
+    <li className={styles.item} key={id}>
+      <span className={spanClass}></span>
+      <img className={styles.img} src={avatar} alt="" width="48" />
+      <p className={styles.text}>{name}</p>
+    </li>
+  );
+};
+
+FriendListItem.defaultProps = {
+  avatar: defaultImg
+};
+
+FriendListItem.propTypes = {
+  id: PropTypes.string,
+  avatar: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
+
 export default FriendListItem;
