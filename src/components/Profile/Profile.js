9369412bfe9ee633bf6ba4ce3218d688dfa4349e
IvettaDashkova <ivettazakonno@gmail.com>
2021-03-19 03:02:20 +0200

    add task 1

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,26 +1,27 @@
 //import React from 'react';
 import PropTypes from 'prop-types';
-import defaultImg from '../components/default.svg';
+import defaultImg from './default.svg';
+import styles from './Profile.module.css'
 
 const Profile = ({ avatar, name, tag, location, stats }) => (
-  <div>
-    <div>
-      <img src={avatar} alt="Аватар пользователя" />
-      <p>{name123}</p>
-      <p>{tag}</p>
-      <p>{location}</p>
+  <div className={styles.profile}>
+    <div className={styles.box}>
+      <img className={styles.img} src={avatar} alt="Аватар пользователя" />
+      <p className={styles.title}>{name}</p>
+      <p className={styles.subtitle}>&#64;{tag}</p>
+      <p className={styles.subtitle}>{location}</p>
     </div>
 
-    <ul>
-      <li>
+    <ul className={styles.list}>
+      <li className={styles.item}>
         <span>Followers</span>
         <span>{stats.followers}</span>
       </li>
-      <li>
+      <li className={styles.item}>
         <span>Views</span>
         <span>{stats.views}</span>
       </li>
-      <li>
+      <li className={styles.item}>
         <span>Likes</span>
         <span>{stats.likes}</span>
       </li>
